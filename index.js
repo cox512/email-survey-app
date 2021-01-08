4b77edc03de68da0ce9d82a82398701ee42d6b36
Jacob Cox <jacob@championofvictory.com>
2021-01-08 16:57:12 -0600

    Added  production keys

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
-
+const keys = require("./config/keys.js");
+require("./models/User");
+require("./services/passport.js");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken:", accessToken);
-      console.log("refreshToken:", refreshToken);
-      console.log("profile:", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
   })
 );
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+app.use(passport.initialize());
+app.use(passport.session());
 
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
 console.log("Listening in on port:", PORT);
